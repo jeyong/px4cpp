56c47741c7af6f84115dd4bad8734560fe8eeee6
Jeyong Shin <jeyong@gmail.com>
2018-11-21 06:28:23 +0900

    update file

--- const/main.cpp
+++ const/main.cpp
@@ -9,7 +9,7 @@ class Base
         int y; 
     public:
         void calculate() { int sum = x + y; }
-        void cal(const int s) { s++; }
+        //void cal(const int s) { s++; }
         //void calculate2() const { x = 2; }
 };
 
